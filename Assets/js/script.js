a29001f8a02ad5552f8b8c73317fd195efc0b614
gisellerx <giselle.armas@outlook.com>
2023-12-02 19:51:03 -0500

    function startQuiz and event listener for it

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,3 +3,9 @@ var startBtn = document.getElementById("start-btn");
 // Hide intro section
 // Start the timer
 // Show the questions
+
+function startQuiz() {
+
+}
+
+startBtn.addEventListener("click", startQuiz);
