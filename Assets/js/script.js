a33fa82bd2284ed1be4d20cf68fc54392cef840c
gisellerx <giselle.armas@outlook.com>
2023-12-02 20:00:56 -0500

    added questions array to script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,9 +4,38 @@ var timerEl = document.getElementById("timer")
 var questionSectionEl = document.getElementById("question-section")
 var timeEl = document.getElementById("title")
 
+var questionsArray = [
+    {
+        title: "q 1",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+    },
+    {
+        title: "q 2",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+    },
+    {
+        title: "q 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+    },
+    {
+        title: "q 4",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+    },
+    {
+        title: "q 5",
+        choices: ["a", "b", "c", "d"],
+        answer: "d"
+    }
+]
+
 // Hide intro section
 // Start the timer
 // Show the questions
+// Data structure for storing questions and choices?
 
 function startQuiz() {
 
